9d3a97de485a4a7f866d8931940955e05d339412
Trung-Nguyen-Chi-24 <trung.nc.63cntt@ntu.edu.vn>
2025-03-07 16:46:53 +0700

    AppCong2
    Xử lý backend

--- AppCong/app/src/main/java/trung/edu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/trung/edu/appcong/MainActivity.java
@@ -1,6 +1,9 @@
 package trung.edu.appcong;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Gắn Layout tương ứng với file này
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+    // Đây là bộ lắng nghe và xử lý sự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view) {
+            // Tìm, tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+      EditText editTextSoA = findViewById(R.id.edtA);
+      EditText editTextSoB = findViewById(R.id.edtB);
+      EditText editTextKetQua = findViewById(R.id.edtKQ);
+      // Lấy dữ liệu về ở điều kiển số a
+      String strA = editTextSoA.getText().toString(); //strA ="2"
+        // Lấy dữ liệu về ở điều kiển số b
+        String strB = editTextSoB.getText().toString(); // strB="4"
+
+        // Chuyển dữ liệu sang dạng số
+      int so_A= Integer.parseInt(strA); //2
+      int so_B= Integer.parseInt(strB); //4
+
+      //Tính toán theo yêu cầu
+
+      int   tong = so_A + so_B; //6
+      String strTong = String.valueOf(tong);     // chuyển sang dạng chuỗi ; "6"
+
+      // Hiện ra màn hình
+      editTextKetQua.setText(strTong);
     }
 }
