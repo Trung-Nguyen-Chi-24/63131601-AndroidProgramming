cf7635f7421aebacfc814fa9699082cff9e811d6
Trung-Nguyen-Chi-24 <trung.nc.63cntt@ntu.edu.vn>
2025-03-20 12:05:17 +0700

    Làm thêm figma Math is Fun!

---
+++ LuyenTap/app/src/main/java/trung/edu/luyentap/MainActivity.java
@@ -0,0 +1,22 @@
+package trung.edu.luyentap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
