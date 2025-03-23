68e0c604a618912628d33d47656f3db46112d546
Trung-Nguyen-Chi-24 <trung.nc.63cntt@ntu.edu.vn>
2025-03-23 14:05:19 +0700

    Làm lại tính tổng

--- AppCong/app/src/main/java/trung/edu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/trung/edu/appcong/MainActivity.java
@@ -16,30 +16,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Gắn Layout tương ứng với file này
         setContentView(R.layout.activity_main);
 
     }
-    // Đây là bộ lắng nghe và xử lý sự kiện click lên nút Tính Tổng
+    // lang nghe va xu ly su kien
     public void XuLyCong(View view) {
-            // Tìm, tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+            // Tìm, tham chiếu đến điều khiển trên tệp XML
       EditText editTextSoA = findViewById(R.id.edtA);
       EditText editTextSoB = findViewById(R.id.edtB);
       EditText editTextKetQua = findViewById(R.id.edtKQ);
       // Lấy dữ liệu về ở điều kiển số a
-      String strA = editTextSoA.getText().toString(); //strA ="2"
-        // Lấy dữ liệu về ở điều kiển số b
-        String strB = editTextSoB.getText().toString(); // strB="4"
+      String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
 
         // Chuyển dữ liệu sang dạng số
-      int so_A= Integer.parseInt(strA); //2
-      int so_B= Integer.parseInt(strB); //4
+      int soA= Integer.parseInt(strA);
+      int soB= Integer.parseInt(strB);
 
       //Tính toán theo yêu cầu
 
-      int   tong = so_A + so_B; //6
-      String strTong = String.valueOf(tong);     // chuyển sang dạng chuỗi ; "6"
-
+      int   tong = soA + soB;
+      String strTong = String.valueOf(tong);     // chuyển sang dạng chuỗi
       // Hiện ra màn hình
       editTextKetQua.setText(strTong);
     }
