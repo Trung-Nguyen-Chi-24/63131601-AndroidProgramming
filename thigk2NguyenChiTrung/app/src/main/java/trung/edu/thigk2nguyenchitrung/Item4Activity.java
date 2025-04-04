f45d35d0c063f98777d1466ad1206a2d88edf484
Trung-Nguyen-Chi-24 <trung.nc.63cntt@ntu.edu.vn>
2025-04-04 15:05:31 +0700

    Thi Giữa kỳ đề 2

---
+++ thigk2NguyenChiTrung/app/src/main/java/trung/edu/thigk2nguyenchitrung/Item4Activity.java
@@ -0,0 +1,23 @@
+package trung.edu.thigk2nguyenchitrung;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Item4Activity extends AppCompatActivity {
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_item4);
+        textView = findViewById(R.id.textView);
+        String NhanItem = getIntent().getStringExtra("selectedItem");
+        textView.setText(NhanItem);
+    }
+}
